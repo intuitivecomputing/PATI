8545e5fc3b32c0ecafb200199026813b91b0f467
Yuxiang Gao <davidgogh96@gmail.com>
2018-09-17 04:05:55 -0400

    update msg composition

--- ropi_tangible_surface/scripts/ropi_robot_inteface/gripper_interface.py
+++ ropi_tangible_surface/scripts/ropi_robot_inteface/gripper_interface.py
@@ -8,3 +8,10 @@ from control_msgs.msg import GripperCommandAction
 class GripperInterface(object):
     def __init__(self, action_name='icl_phri_gripper/gripper_controller'):
         self.client = actionlib.SimpleActionClient(action_name, GripperCommandAction)
+        self.client.wait_for_server(rospy.Duration(10))
+        
+
+    def close(self):
+        self.new_goal = GripperCommandGoal()
+        self.new_goal.command.position = 0.0
+        self.new_goal
